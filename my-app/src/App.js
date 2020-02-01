960338faefcd2f8bffbb716a631eac8226d08314
rahubhardwaj <rahubhardwaj@expediagroup.com>
2020-02-01 15:23:06 +0530

    my second_2 commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person.js'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi this is function</h1>
-    </div>
-  );
-}
-
 class App1 extends Component {
+    state = {
+        persons: [
+            { name: 'test', age: 28 },
+            { name: 'test1', age: 26 },
+            { name: 'test2', age: 27 }
+        ]
+    }
+
     render(){
         return (
           <div className="App1">
                 <h1>Hi This is testing</h1>
                 <button>Switch User</button>
-                <Person name='Vansh' age='24'/>
-                <Person name='Rahul' age='27'/>
-                <Person name='chiku' age='1000' >what you want to say ?</Person>
+                <Person name={this.state.persons[0]} age={this.state.age[0]}/>
+                <Person name={this.state.persons[1]} age={this.state.age[1]}/>
+                <Person name={this.state.persons[1]} age={this.state.age[2]} >what you want to say ?</Person>
           </div>
         );
     }
