19ed1bb3897ddea6ae8e631b6d1becb9a80b7da9
rahubhardwaj <rahubhardwaj@expediagroup.com>
2020-02-01 12:58:49 +0530

    my second commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,28 @@
-import React , { Component } from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person.js'
 
-class App extends Component{
+function App() {
   return (
     <div className="App">
-      <h1>Hi This is testing</h1>
-        <button>Switch User</button>
-      <Person name='Vansh' age='24'/>
-      <Person name='Rahul' age='27'/>
-      <Person name='chiku' age='1000' >what you want to say ?</Person>
+      <h1>Hi this is function</h1>
     </div>
   );
+}
+
+class App1 {
+    render(){
+        return (
+          <div className="App1">
+                <h1>Hi This is testing</h1>
+                <button>Switch User</button>
+                <Person name='Vansh' age='24'/>
+                <Person name='Rahul' age='27'/>
+                <Person name='chiku' age='1000' >what you want to say ?</Person>
+          </div>
+        );
+    }
 }
 
 export default App;
