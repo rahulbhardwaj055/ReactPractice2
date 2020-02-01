d5ff0c0ede2264b8cb3a66b0100386db3e91d4f4
rahubhardwaj <rahubhardwaj@expediagroup.com>
2020-02-01 12:36:12 +0530

    my first commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,10 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person.js'
 
-function App() {
+class App extends Component{
   return (
     <div className="App">
       <h1>Hi This is testing</h1>
+        <button>Switch User</button>
       <Person name='Vansh' age='24'/>
       <Person name='Rahul' age='27'/>
       <Person name='chiku' age='1000' >what you want to say ?</Person>
