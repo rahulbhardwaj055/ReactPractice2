abcb47a429507f0d4ad87d29475302d7fcb16d02
rahubhardwaj <rahubhardwaj@expediagroup.com>
2020-02-01 11:52:04 +0530

    my first commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,7 @@
 import React , { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import './Person/Person.js'
+import Person from './Person/Person.js'
 
 function App() {
   return (
