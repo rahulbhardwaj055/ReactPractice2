b04f82a3b8607054d25882beb38922dcb5debcda
rahubhardwaj <rahubhardwaj@expediagroup.com>
2020-02-01 11:41:36 +0530

    my first commit

---
+++ my-app/src/Person/Person.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const person = (props) => {
+    return (
+        <div>
+            <h1>Hi this is testing part two</h1>
+        </div>
+
+    )
+
+};
+
+export default person;
